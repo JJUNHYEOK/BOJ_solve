dc412b70d3ef7d5333f8503a3dbd0099c807c73d
Jun Hyeok Jeong <148296782+JJUNHYEOK@users.noreply.github.com>
2024-07-20 21:24:44 +0900

    클린코드 추가.py

--- 백준/Bronze/11179. 2진수 뒤집기/2진수 뒤집기.py
+++ 백준/Bronze/11179. 2진수 뒤집기/2진수 뒤집기.py
@@ -7,3 +7,11 @@ bin_reversed = bin[::-1]
 dec = int(bin_reversed,2)
 
 print(dec)
+
+
+--------------------------------------------
+
+[clean code]
+import sys
+n = int(sys.stdin.readline())
+print(int(bin(n)[2:][::-1],2))
