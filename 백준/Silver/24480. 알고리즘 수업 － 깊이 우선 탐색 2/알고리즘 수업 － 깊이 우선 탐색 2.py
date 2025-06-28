a257b8ca04044648d141c936f5a0a11e0cdc135f
Jun Hyeok Jeong <148296782+JJUNHYEOK@users.noreply.github.com>
2025-06-28 21:31:44 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 676 ms, Memory: 70072 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.py
@@ -0,0 +1,37 @@
+import sys
+
+sys.setrecursionlimit(200000)
+input = sys.stdin.readline
+
+n, m, r = map(int, input().split())  # vertex, edge, start
+
+graph = [[] for _ in range(n + 1)]  # 평소와 동일
+visited = [False] * (n + 1)  # 평소와 동일
+order = [0] * (n + 1)  # vertex i의 방문 순서 번호
+cnt = 1  # 현재까지 방문한 vertex 개수
+
+for _ in range(m):
+  a, b = map(int, input().split())
+
+  graph[a].append(b)
+  graph[b].append(a)
+
+for i in range(1, n + 1):
+  graph[i].sort(reverse=True)
+
+
+def dfs(node):
+  global cnt
+  visited[node] = True
+  order[node] = cnt
+  cnt += 1
+
+  for neighbor in graph[node]:
+    if not visited[neighbor]:
+      dfs(neighbor)
+
+
+dfs(r)
+
+for i in range(1, n + 1):
+  print(order[i])
