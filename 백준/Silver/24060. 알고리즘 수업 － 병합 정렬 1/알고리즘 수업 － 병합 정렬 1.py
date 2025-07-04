9e356a728f2b29e5f8ed9dec34c5877f31435d0f
Jun Hyeok Jeong <148296782+JJUNHYEOK@users.noreply.github.com>
2025-07-04 16:28:39 +0900

    [Silver III] Title: 알고리즘 수업 - 병합 정렬 1, Time: 2848 ms, Memory: 90692 KB -BaekjoonHub

---
+++ 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.py
@@ -0,0 +1,62 @@
+import sys
+sys.setrecursionlimit(2000000)
+
+input = sys.stdin.readline
+
+
+
+def merge_sort(arr, p, r):
+  if p<r :
+    q = (p+r) // 2
+    merge_sort(arr, p, q)
+    merge_sort(arr, q+1, r)
+    merge(arr, p, q, r)
+
+def merge(arr, p, q, r):
+  global cnt, result
+
+  i = p 
+  j = q+1
+  tmp = []
+
+  while i<=q and j<=r:
+    if arr[i] <= arr[j]:
+      tmp.append(arr[i])
+      i += 1
+
+    else:
+      tmp.append(arr[j])
+      j += 1
+
+  while i<=q:
+    tmp.append(arr[i])
+    i += 1
+
+  while j<=r:
+    tmp.append(arr[j])
+    j += 1
+
+
+  i = p
+  t = 0
+
+  while i<=r :
+    arr[i] = tmp[t]
+    cnt += 1
+
+    if cnt == k:
+      result = arr[i]
+      break
+
+    i += 1
+    t += 1
+    
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
+
+cnt = 0
+result = -1
+
+merge_sort(arr, 0, n-1)
+
+print(result)
