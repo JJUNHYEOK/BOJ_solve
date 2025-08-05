f88e0740348acf8b43ab84e2b0a88ba4c8a0c4c9
Jun Hyeok Jeong <148296782+JJUNHYEOK@users.noreply.github.com>
2025-08-05 18:46:02 +0900

    [Silver II] Title: DFS와 BFS, Time: 64 ms, Memory: 35700 KB -BaekjoonHub

---
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -0,0 +1,47 @@
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+
+n, m, v = map(int, input().strip().split())
+
+graph = [[] for _ in range(n+1)]
+visited = [False]*(n+1)
+
+for _ in range(m):
+    a, b = map(int,input().strip().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+for i in range(1, n+1):
+    graph[i].sort()
+
+def dfs(graph, v, visited):
+    visited[v] = True
+    print(v, end=' ')
+
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+dfs(graph, v, visited)
+print()
+
+
+visited = [False]*(n+1)
+
+def bfs(graph, start, visited):
+    queue = deque([start])
+    visited[start] = True
+
+    while queue:
+        v = queue.popleft()
+        print(v, end=' ')
+
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+
+bfs(graph, v, visited)
