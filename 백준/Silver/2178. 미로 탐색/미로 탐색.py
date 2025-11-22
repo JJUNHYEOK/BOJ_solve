aca6bdfc81868e23a8d25046fa51e275d0ff2c7e
Junhyeok Jeong <148296782+JJUNHYEOK@users.noreply.github.com>
2025-11-22 17:24:58 +0900

    [Silver I] Title: 미로 탐색, Time: 64 ms, Memory: 34968 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,29 +1,37 @@
+import sys
 from collections import deque
 
-n, m = map(int, input().split())
+input = sys.stdin.readline
+
+n,m = map(int,input().split())
 graph = [list(map(int,input().strip())) for _ in range(n)]
 
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-def bfs(x,y):
+visited = [[False]*m for _ in range(n)]
+cnt = 0
+
+def bfs(x, y):
+    global cnt
     q = deque()
     q.append((x,y))
+    visited[x][y] = True
+    dis = [[0]*m for _ in range(n)]
+    dis[x][y] = 1
 
     while q:
         x, y = q.popleft()
-        
+
         for i in range(4):
             nx, ny = x+dx[i], y+dy[i]
-            if nx<0 or ny<0 or nx >= n or ny >= m:
-                continue
-            if graph[nx][ny] == 0:
-                continue
-            if graph[nx][ny] == 1:
-                graph[nx][ny] = graph[x][y] + 1
-                q.append((nx,ny))
+
+            if 0 <= nx < n and 0 <= ny < m:
+                if graph[nx][ny] == 1 and dis[nx][ny] == 0:
+                    dis[nx][ny] = dis[x][y] + 1
+                    q.append((nx,ny))
 
     
-    return graph[n-1][m-1]
+    return dis[n-1][m-1]
 
 print(bfs(0,0))
