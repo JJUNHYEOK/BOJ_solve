b404e3505227a42f3dd29757e8b035d9b47bdff1
Junhyeok Jeong <148296782+JJUNHYEOK@users.noreply.github.com>
2026-01-06 16:59:49 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 220 ms, Memory: 41144 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -4,11 +4,14 @@ input = sys.stdin.readline
 
 n, m = map(int,input().split())
 stk = list(map(int,input().split()))
-stksum = [0 for _ in range(n+1)]
+mem = [0 for _ in range(n+1)]
 
 for i in range(1, n+1):
-    stksum[i] = stksum[i-1] + stk[i-1]
+    mem[i] = mem[i-1] + stk[i-1]
 
 for _ in range(m):
     a, b = map(int,input().split())
-    print(stksum[b] - stksum[a-1])
+    print(mem[b] - mem[a-1])
+
+
+## 첫주부터 DP ..?
