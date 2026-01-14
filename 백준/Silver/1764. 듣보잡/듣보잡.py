e03cd32ff0316188d85b062fe1d4df7f868d51cf
Junhyeok Jeong <148296782+JJUNHYEOK@users.noreply.github.com>
2026-01-15 00:01:27 +0900

    [Silver IV] Title: 듣보잡, Time: 80 ms, Memory: 44192 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -0,0 +1,23 @@
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int,input().split())
+
+unseen = set()
+unheard = set()
+
+for _ in range(n):
+    name = input().rstrip()
+    unseen.add(name)
+
+for _ in range(m):
+    name = input().rstrip()
+    unheard.add(name)
+
+result = sorted(set(unseen&unheard))
+
+print(len(result))
+
+for name in result:
+    print(name)
