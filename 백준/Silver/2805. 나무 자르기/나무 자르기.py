d0034e27702ad6f18ddfdbc0818d0ae2c87187d1
Junhyeok Jeong <148296782+JJUNHYEOK@users.noreply.github.com>
2026-01-26 23:37:26 +0900

    [Silver II] Title: 나무 자르기, Time: 4740 ms, Memory: 148224 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -0,0 +1,27 @@
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int,input().split())
+wood = list(map(int,input().split()))
+
+start = 0
+end = max(wood)
+result = 0
+
+while(start <= end):
+    total = 0
+    mid = (start+end)//2
+
+    for num in wood:
+        if num > mid:
+            total += num - mid
+
+    if total < m:
+        end = mid - 1
+
+    else:
+        result = mid
+        start = mid + 1
+
+print(result)
