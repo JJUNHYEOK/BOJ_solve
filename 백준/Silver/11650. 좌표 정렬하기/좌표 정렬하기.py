ca0168c8fef926038d1c7e7c3fc9dc782775188a
Junhyeok Jeong <148296782+JJUNHYEOK@users.noreply.github.com>
2026-01-27 15:54:20 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 524 ms, Memory: 46504 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -1,13 +1,44 @@
+import sys
+
+input = sys.stdin.readline
+
 n = int(input())
-number = []
+locate = []
 
 for _ in range(n):
-    num = list(map(int,(input().split())))
-    number.append(num)
+    a, b = map(int,input().split())
+    locate.append((a,b))
 
-number.sort()
+def quick_sort(arr, start, end):
+    if start >= end:
+        return
+    
+    pivot = (start + end) // 2
+    arr[start], arr[pivot] = arr[pivot], arr[start]
+    pivot = start
+    left = start + 1
+    right = end
 
+    while left <= right:
+        while left <= end and arr[left] <= arr[pivot]:
+            left += 1
+        
+        while right > start and arr[right] >= arr[pivot]:
+            right -= 1
 
-for x,y in number:
-  print(x,y)
+        if left > right:
+            arr[right], arr[pivot] = arr[pivot], arr[right]
 
+        else:
+            arr[left], arr[right] = arr[right], arr[left]
+
+    
+    quick_sort(arr, start, right-1)
+    quick_sort(arr, right+1, end)
+
+    return arr
+
+quick_sort(locate, 0, len(locate)-1)
+
+for x, y in locate:
+    print(x,y)
