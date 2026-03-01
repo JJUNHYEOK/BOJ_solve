dd42a16eba0c120454d97b290117ef71ecd1c8b7
Junhyeok Jeong <148296782+JJUNHYEOK@users.noreply.github.com>
2026-03-02 00:40:18 +0900

    [Gold V] Title: 토마토, Time: 1300 ms, Memory: 100464 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -4,37 +4,39 @@ from collections import deque
 input = sys.stdin.readline
 
 m, n = map(int,input().split())
-graph = [list(map(int,input().split())) for _ in range(n)]
-max_date = 0
 
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
+graph = [list(map(int,input().split())) for _ in range(n)]
+md = 0
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
 def bfs(graph):
     q = deque()
-
+    
     for i in range(n):
         for j in range(m):
             if graph[i][j] == 1:
-                q.append((i,j))
+                q.append((i, j))
 
     while q:
         x, y = q.popleft()
+        
         for i in range(4):
             nx, ny = x+dx[i], y+dy[i]
+
             if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 0:
                 graph[nx][ny] = graph[x][y] + 1
                 q.append((nx, ny))
-
 
 bfs(graph)
 
 for i in range(n):
     for j in range(m):
         if graph[i][j] == 0:
-            print("-1")
+            print('-1')
             exit()
-        
-        max_date = max(max_date, graph[i][j])
-        
-print(max_date-1)
+
+        md = max(md, graph[i][j])
+
+print(md-1)
