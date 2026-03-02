4d45bfa29ed33136e02656622ba9ba6a1b9cc592
Junhyeok Jeong <148296782+JJUNHYEOK@users.noreply.github.com>
2026-03-02 10:58:30 +0900

    [Silver I] Title: 미로 탐색, Time: 64 ms, Memory: 34968 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -3,35 +3,34 @@ from collections import deque
 
 input = sys.stdin.readline
 
-n,m = map(int,input().split())
-graph = [list(map(int,input().strip())) for _ in range(n)]
+n, m = map(int,input().split())
+graph = [list(map(int,input().rstrip())) for _ in range(n)]
+visited = [[False]*m for _ in range(n)]
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-visited = [[False]*m for _ in range(n)]
-cnt = 0
-
 def bfs(x, y):
-    global cnt
     q = deque()
-    q.append((x,y))
+    q.append((x, y))
     visited[x][y] = True
-    dis = [[0]*m for _ in range(n)]
-    dis[x][y] = 1
+    graph[x][y] = 1
 
     while q:
+
         x, y = q.popleft()
 
         for i in range(4):
             nx, ny = x+dx[i], y+dy[i]
 
             if 0 <= nx < n and 0 <= ny < m:
-                if graph[nx][ny] == 1 and dis[nx][ny] == 0:
-                    dis[nx][ny] = dis[x][y] + 1
-                    q.append((nx,ny))
+                if graph[nx][ny] == 1 and not visited[nx][ny]:
+                    graph[nx][ny] = graph[x][y] + 1
+                    q.append((nx, ny))
 
-    
-    return dis[n-1][m-1]
+bfs(0, 0)
 
-print(bfs(0,0))
+ans = 0
+ans = graph[n-1][m-1]
+
+print(ans)
