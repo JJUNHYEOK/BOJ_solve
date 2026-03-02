c5aa5c7684799f1852ec087f2f9943a617c447c1
Junhyeok Jeong <148296782+JJUNHYEOK@users.noreply.github.com>
2026-03-02 11:22:48 +0900

    [Silver I] Title: 그림, Time: 288 ms, Memory: 36504 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.py
+++ 백준/Silver/1926. 그림/그림.py
@@ -1,36 +1,44 @@
 import sys
-sys.setrecursionlimit(10**6)
+from collections import deque
 
 input = sys.stdin.readline
 
 n, m = map(int,input().split())
 graph = [list(map(int,input().split())) for _ in range(n)]
-visited = [[False]*(m) for _ in range(n)]
-mx = 0
-cnt = 0
+visited = [[False]*m for _ in range(n)]
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-def dfs(x, y):
+def bfs(x, y):
+    q = deque()
+    q.append((x, y))
+    visited[x][y] = True
     area = 1
-    visited[x][y] = 1
 
-    for i in range(4):
-        nx, ny = x+dx[i], y+dy[i]
+    while q:
+        x, y = q.popleft()
 
-        if 0 <= nx < n and 0 <= ny < m:
-            if not visited[nx][ny] and graph[nx][ny] == 1:
-                area += dfs(nx, ny)
+        for i in range(4):
+            nx, ny = x+dx[i], y+dy[i]
+
+            if 0 <= nx < n and 0 <= ny < m:
+                if graph[nx][ny] == 1 and not visited[nx][ny]:
+                    visited[nx][ny] = True
+                    q.append((nx, ny))
+                    area += 1
 
     return area
+
+cnt = 0
+maxarea = 0
 
 for i in range(n):
     for j in range(m):
         if graph[i][j] == 1 and not visited[i][j]:
+            area = bfs(i, j)
             cnt += 1
-            area = dfs(i, j)
-            mx = max(mx, area)
+            maxarea = max(maxarea, area)
 
 print(cnt)
-print(mx)
+print(maxarea)
