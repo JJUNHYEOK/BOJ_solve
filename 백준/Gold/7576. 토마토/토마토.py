ac79d09359437449aa529f6c634f6cc7edb2bd2b
Junhyeok Jeong <148296782+JJUNHYEOK@users.noreply.github.com>
2026-03-10 00:27:17 +0900

    [Gold V] Title: 토마토, Time: 1240 ms, Memory: 100492 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -4,39 +4,41 @@ from collections import deque
 input = sys.stdin.readline
 
 m, n = map(int,input().split())
-
 graph = [list(map(int,input().split())) for _ in range(n)]
-md = 0
+
+q = deque()
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-def bfs(graph):
-    q = deque()
-    
-    for i in range(n):
-        for j in range(m):
-            if graph[i][j] == 1:
-                q.append((i, j))
+for i in range(n):
+    for j in range(m):
+        if graph[i][j] == 1:
+            q.append((i, j))
+
+def bfs():
+    global q
 
     while q:
         x, y = q.popleft()
-        
+
         for i in range(4):
             nx, ny = x+dx[i], y+dy[i]
 
-            if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 0:
-                graph[nx][ny] = graph[x][y] + 1
-                q.append((nx, ny))
+            if 0 <= nx < n and 0 <= ny < m:
+                if graph[nx][ny] == 0:
+                    graph[nx][ny] = graph[x][y] + 1
+                    q.append((nx, ny))
 
-bfs(graph)
+bfs()
+ans = 0
 
-for i in range(n):
-    for j in range(m):
-        if graph[i][j] == 0:
-            print('-1')
-            exit()
+for row in graph:
+    for x in row:
+        if x == 0:
+            print(-1)
+            exit(0)
 
-        md = max(md, graph[i][j])
+        ans = max(ans, x)
 
-print(md-1)
+print(ans - 1)
