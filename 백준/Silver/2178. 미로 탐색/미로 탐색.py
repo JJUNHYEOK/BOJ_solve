6dae8081d7a1744789958376609b87e892363611
Junhyeok Jeong <148296782+JJUNHYEOK@users.noreply.github.com>
2026-03-10 00:09:57 +0900

    [Silver I] Title: 미로 탐색, Time: 68 ms, Memory: 34936 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -5,32 +5,29 @@ input = sys.stdin.readline
 
 n, m = map(int,input().split())
 graph = [list(map(int,input().rstrip())) for _ in range(n)]
-visited = [[False]*m for _ in range(n)]
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-def bfs(x, y):
+
+def bfs(sx, sy):
     q = deque()
-    q.append((x, y))
-    visited[x][y] = True
-    graph[x][y] = 1
+    q.append((sx, sy))
 
     while q:
 
-        x, y = q.popleft()
+        x,y = q.popleft()
 
         for i in range(4):
             nx, ny = x+dx[i], y+dy[i]
 
             if 0 <= nx < n and 0 <= ny < m:
-                if graph[nx][ny] == 1 and not visited[nx][ny]:
-                    graph[nx][ny] = graph[x][y] + 1
+                if graph[nx][ny] == 1:
                     q.append((nx, ny))
+                    graph[nx][ny] = graph[x][y] + 1
 
-bfs(0, 0)
+    return graph
 
-ans = 0
-ans = graph[n-1][m-1]
+bfs(0,0)
 
-print(ans)
+print(graph[n-1][m-1])
