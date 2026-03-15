c2284d12cb571adedfb95965e99c2bf700a4777c
Junhyeok Jeong <148296782+JJUNHYEOK@users.noreply.github.com>
2026-03-15 16:39:42 +0900

    [Gold IV] Title: 뱀, Time: 60 ms, Memory: 35004 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.py
+++ 백준/Gold/3190. 뱀/뱀.py
@@ -1,57 +1,54 @@
 import sys
 from collections import deque
-
 input = sys.stdin.readline
 
 n = int(input())
 graph = [[0]*n for _ in range(n)]
-time = 0
-sr, sc = 0, 0 # 초기 출발 위치는 fix
-graph[sr][sc] = -1 # 뱀이 처음 출발하는 곳, 길어지면 다른 곳들도 -1로 전파됨.
 
 k = int(input())
 
 for _ in range(k):
     r, c = map(int,input().split())
-    graph[r-1][c-1] = 2 # 사과 있는 곳은 2로 표현
+    graph[r-1][c-1] = 2
 
 l = int(input())
-
-direction = {}
+dir = {}
 
 for _ in range(l):
     x, ch = input().split()
-    direction[int(x)] = ch
+    dir[int(x)] = ch
 
-d = 0 # 오른쪽 방향 벡터
-dx = [0, 1, 0, -1]
-dy = [1, 0, -1, 0]
-
+time = 0
+sr, sc = 0, 0
+graph[sr][sc] = -1
 snake = deque()
 snake.append((sr, sc))
 
+d = 0
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+
 while True:
-    
     time += 1
 
     nx = sr + dx[d]
     ny = sc + dy[d]
 
-    # 벽 충돌 ㅋㅋ
+    # 벽 충돌
     if nx < 0 or nx >= n or ny < 0 or ny >= n:
         break
 
-    # 몸빵
+    # 자기 몸 만났을 때 ㅠ ㅠ
     if graph[nx][ny] == -1:
         break
 
-    # 이동해라 ~
+    # 이동해보자
     snake.append((nx, ny))
 
     # 사과 처리
     if graph[nx][ny] == 2:
         graph[nx][ny] = -1
-    
+
     else:
         tx, ty = snake.popleft()
         graph[tx][ty] = 0
@@ -59,11 +56,13 @@ while True:
     graph[nx][ny] = -1
     sr, sc = nx, ny
 
-    # 방향 바꿔야지
-    if time in direction:
+    # 방향 전환
 
-        if direction[time] == 'L':
+    if time in dir:
+
+        if dir[time] == 'L':
             d = (d+3)%4
+
         else:
             d = (d+1)%4
 
