536f6cedcc20c0a3aa40c718f440bde13af4d83e
Junhyeok Jeong <148296782+JJUNHYEOK@users.noreply.github.com>
2026-03-18 00:47:58 +0900

    [Gold IV] Title: 뱀, Time: 64 ms, Memory: 35004 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.py
+++ 백준/Gold/3190. 뱀/뱀.py
@@ -1,69 +1,57 @@
 import sys
 from collections import deque
-input = sys.stdin.readline
 
 n = int(input())
-graph = [[0]*n for _ in range(n)]
-
 k = int(input())
+g = [[0]*n for _ in range(n)]
 
 for _ in range(k):
-    r, c = map(int,input().split())
-    graph[r-1][c-1] = 2
+    sx, sy = map(int,input().split())
+    g[sx-1][sy-1] = 2
 
 l = int(input())
+
 dir = {}
 
 for _ in range(l):
-    x, ch = input().split()
-    dir[int(x)] = ch
+    x, c = input().split()
+    dir[int(x)] = c
 
-time = 0
-sr, sc = 0, 0
-graph[sr][sc] = -1
-snake = deque()
-snake.append((sr, sc))
-
-d = 0
 dx = [0, 1, 0, -1]
 dy = [1, 0, -1, 0]
 
+time = 0
+xx, yy = 0, 0
+g[xx][yy] = 1
+d = 0
+q = deque()
+q.append((xx, yy))
+
 while True:
     time += 1
-
-    nx = sr + dx[d]
-    ny = sc + dy[d]
-
-    # 벽 충돌
-    if nx < 0 or nx >= n or ny < 0 or ny >= n:
+    nx, ny = xx+dx[d], yy+dy[d]
+    
+    if nx < 0 or nx >= n or ny < 0 or ny >= n or g[nx][ny] == 1:
         break
-
-    # 자기 몸 만났을 때 ㅠ ㅠ
-    if graph[nx][ny] == -1:
-        break
-
-    # 이동해보자
-    snake.append((nx, ny))
-
-    # 사과 처리
-    if graph[nx][ny] == 2:
-        graph[nx][ny] = -1
 
     else:
-        tx, ty = snake.popleft()
-        graph[tx][ty] = 0
-
-    graph[nx][ny] = -1
-    sr, sc = nx, ny
-
-    # 방향 전환
-
-    if time in dir:
-
-        if dir[time] == 'L':
-            d = (d+3)%4
+        if g[nx][ny] == 2:
+            g[nx][ny] = 1
+            q.append((nx, ny))
 
         else:
-            d = (d+1)%4
+            g[nx][ny] = 1
+            q.append((nx, ny))
+            ox, oy = q.popleft()
+            g[ox][oy] = 0
+
+    if time in dir: # KeyError 방지용
+                if dir[time] == 'L':
+                    d = (d+3)%4
+
+                else:
+                    d = (d+1)%4
+    
+    xx, yy = nx, ny
 
 print(time)
