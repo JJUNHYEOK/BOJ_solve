e2a0a4596a702053c767de7a848a8f5def4d3ada
Junhyeok Jeong <148296782+JJUNHYEOK@users.noreply.github.com>
2026-03-21 01:37:44 +0900

    [Gold III] Title: 불!, Time: 1244 ms, Memory: 72500 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.py
+++ 백준/Gold/4179. 불！/불！.py
@@ -11,43 +11,6 @@ dy = [0, 0, -1, 1]
 
 fire_time = [[-1]*C for _ in range(R)]
 
-for i in range(R):
-    for j in range(C):
-        if g[i][j] == 'J':
-            si, sj = i, j
-
-def bfs():
-    q = deque()
-    dist = [[-1]*C for _ in range(R)]
-    q.append((si, sj))
-    dist[si][sj] = 0
-
-    while q:
-
-        x, y = q.popleft()
-
-        if x == 0 or x == R-1 or y == 0 or y == C-1:
-            return dist[x][y] + 1
-
-        for i in range(4):
-            nx = x+dx[i]
-            ny = y+dy[i]
-
-            if nx < 0 or nx >= R or ny < 0 or ny >= C:
-                continue
-            if g[nx][ny] == '#':
-                continue
-            if dist[nx][ny] != -1:
-                continue
-
-            if fire_time[nx][ny] != -1 and fire_time[nx][ny] <= dist[x][y] + 1:
-                continue
-
-            dist[nx][ny] = dist[x][y] + 1
-            q.append((nx, ny))
-    
-    return "IMPOSSIBLE"
-
 def fire():
     q = deque()
 
@@ -58,17 +21,55 @@ def fire():
                 fire_time[i][j] = 0
 
     while q:
-        x, y  = q.popleft()
-        
+
+        x, y = q.popleft()
+
         for i in range(4):
             nx, ny = x+dx[i], y+dy[i]
 
-            if nx < 0 or nx >= R or ny < 0 or ny >= C or g[nx][ny] == '#':
+            if nx < 0 or nx >= R or ny < 0 or ny >= C: # 범위 체크가 항상 1번
+                continue
+            if g[nx][ny] == '#':
+                continue
+            if fire_time[nx][ny] != -1:
                 continue
 
-            if fire_time[nx][ny] == -1:
-                fire_time[nx][ny] = fire_time[x][y] + 1
-                q.append((nx,ny))
+            fire_time[nx][ny] = fire_time[x][y] + 1
+            q.append((nx, ny))
+
+def bfs():
+    q = deque()
+    dis = [[-1]*C for _ in range(R)]
+
+    for i in range(R):
+        for j in range(C):
+            if g[i][j] == 'J':
+                q.append((i, j))
+                dis[i][j] = 0
+
+    while q:
+
+        x, y = q.popleft()
+
+        if x == 0 or x == R-1 or y == 0 or y == C-1: # 탈출 조건 !!!@#@!#
+            return dis[x][y] + 1
+
+        for i in range(4):
+            nx, ny = x+dx[i], y+dy[i]
+
+            if nx < 0 or nx >= R or ny < 0 or ny >= C:
+                continue
+            if g[nx][ny] == '#':
+                continue
+            if dis[nx][ny] != -1:
+                continue
+            if fire_time[nx][ny] != -1 and fire_time[nx][ny] <= dis[x][y] + 1:
+                continue
+
+            dis[nx][ny] = dis[x][y] + 1
+            q.append((nx, ny))
+
+    return "IMPOSSIBLE"
 
 fire()
 print(bfs())
