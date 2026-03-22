ebb4471c3b7f194e2a0564a10eed7a55f6d51430
Junhyeok Jeong <148296782+JJUNHYEOK@users.noreply.github.com>
2026-03-22 23:27:05 +0900

    [Silver I] Title: 전쟁 - 전투, Time: 64 ms, Memory: 34984 KB -BaekjoonHub

--- 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.py
+++ 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.py
@@ -9,63 +9,37 @@ g = [list(input().strip()) for _ in range(N)]
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-def ally(i, j):
+
+def bfs(i, j, team):
     q = deque()
     q.append((i, j))
     g[i][j] = 0
     cnt = 1
 
     while q:
-        ax, ay = q.popleft()
+        x, y = q.popleft()
 
         for d in range(4):
-            nax = ax+dx[d]
-            nay = ay+dy[d]
+            nx = x + dx[d]
+            ny = y + dy[d]
 
-            if 0 <= nax < N and 0 <= nay < M:
-                if g[nax][nay] == 'W':
-                    q.append((nax, nay))
-                    g[nax][nay] = 0
+            if 0 <= nx < N and 0 <= ny < M:
+                if g[nx][ny] == team:
+                    q.append((nx, ny))
+                    g[nx][ny] = 0
                     cnt += 1
 
     return cnt
 
-def enemy(i, j):
-    q = deque()
-    q.append((i, j))
-    g[i][j] = 0
-    cnt = 1
-
-    while q:
-        ex, ey = q.popleft()
-
-        for d in range(4):
-            nex = ex+dx[d]
-            ney = ey+dy[d]
-
-            if 0 <= nex < N and 0 <= ney < M:
-                if g[nex][ney] == 'B':
-                    q.append((nex, ney))
-                    g[nex][ney] = 0
-                    cnt += 1
-    
-    return cnt
-
-cnt_a = 0
-ans_a = 0
-cnt_b = 0
-ans_b = 0
+ans_w, ans_b = 0, 0
 
 for i in range(N):
     for j in range(M):
         if g[i][j] == 'W':
-            cnt_a = ally(i, j)
-            ans_a += cnt_a**2
+            cnt = bfs(i, j, 'W')
+            ans_w += cnt**2
+        elif g[i][j] == 'B':
+            cnt = bfs(i, j, 'B')
+            ans_b += cnt**2
 
-for i in range(N):
-    for j in range(M):
-        if g[i][j] == 'B':
-            cnt_b = enemy(i, j)
-            ans_b += cnt_b**2
-
-print(ans_a, ans_b)
+print(ans_w, ans_b)
