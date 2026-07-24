ee9874df9156ffc521550befff0e1b9ec92b8167
Junhyeok Jeong <148296782+JJUNHYEOK@users.noreply.github.com>
2026-07-24 22:32:12 +0900

    [level 2] Title: 더 맵게, Time: 1650.14 ms, Memory: 52.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -0,0 +1,20 @@
+import heapq
+
+def solution(scoville, K):
+    ans = 0
+
+    heapq.heapify(scoville)
+
+    while scoville[0] < K:
+        if len(scoville) < 2:
+            return -1
+
+        first = heapq.heappop(scoville)
+        second = heapq.heappop(scoville)
+
+        new = first + (second*2)
+        heapq.heappush(scoville, new)
+
+        ans += 1
+
+    return ans
